299e146d1a2f353db4d4e8403a472a4ba8cb69cd
wicnewyen <133418529+wicnewyen@users.noreply.github.com>
2024-08-15 23:14:32 -0400

    Add functionality to calculator - weight now displays

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 import './App.css'
 
 function calculatePlatesKG(weight) {
@@ -30,7 +30,9 @@ function calculatePlatesKG(weight) {
 }
 
 function calculatePlatesLB(weight) {
-  
+
+  const barWeight = 45;
+
   const plates = [
     { weight: 45, color: '#646cff' },
     { weight: 35, color: '#646cff' },
@@ -59,12 +61,46 @@ function calculatePlatesLB(weight) {
 function App() {
   const [weight, setWeight] = useState(135)
   const [unit, setUnit] = useState('lb')
+  const [plates, setPlates] = useState([{weight: 45, color: '#646cff'}])
+
+  useEffect(() => {
+    if (unit === 'kg') {
+      setPlates(calculatePlatesKG(weight));
+    } else {
+      setPlates(calculatePlatesLB(weight));
+    }
+  }, [weight, unit]);
+
+  const handleKgChange = (e) => {
+    const value = parseFloat(e.target.value);
+    setWeight(value);
+    setUnit('kg');
+  };
+
+  const handleLbChange = (e) => {
+    const value = parseFloat(e.target.value);
+    setWeight(value);
+    setUnit('lb');
+  };
+
+  const getTextColor = (backgroundColor) => {
+    return backgroundColor === 'white' ? 'black' : 'white';
+  };
 
   return (
     <>
       <div className="barbell">
-        <div className="plate-left"></div>
-        <div className="plate-right"></div>
+        <div className="leftPlates">
+          {plates.map((plate, index) => (
+            <div key={index} className = "plate-left" style={{ backgroundColor: plate.color, color: getTextColor(plate.color)}}> {plate.weight} </div>
+          ))}
+        </div>
+        <div className="rightPlates">
+          {plates.map((plate, index) => (
+            <div key={index} className = "plate-right" style={{ backgroundColor: plate.color, color: getTextColor(plate.color)}}> {plate.weight}</div>
+          )).reverse()}
+        </div>
+          
       </div>
       
       <h1>powerwictor</h1>
